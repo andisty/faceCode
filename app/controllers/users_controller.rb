0db3b4986be62d0a4eef61be5568b8e98bc7ab14
Vincent Brinkman <vincentbrinkman@yahoo.com>
2016-06-24 15:11:35 +0200

    profilepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,6 @@ def index
   @users = User.all
 end
   def show
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:id])
   end
  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
 
-  
   get 'user_posts/:user_id' => 'posts#user', as: :user_posts
 
   get 'face_code/home'

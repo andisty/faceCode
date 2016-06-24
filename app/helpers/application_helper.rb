0e2588b40d86e0a24452f1480c99b97e7dd51620
Vincent Brinkman <vincentbrinkman@yahoo.com>
2016-06-24 15:39:07 +0200

    profilepage2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def gravatar_for(user, options = { size: 80})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar_url = "http://www.adweek.com/socialtimes/files/2014/08/anonymous.jpg"
     image_tag(gravatar_url, alt: user.email)
   end
 end
